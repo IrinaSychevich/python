2fdd26e1317498a22651abdb7d7d6eb0befa52e0
IrinaSychevich <105856175+IrinaSychevich@users.noreply.github.com>
2022-05-25 20:08:55 +0300

    Delete 1.2.py

--- 1.2.py
+++
@@ -1,7 +0,0 @@
-time = int(input('Введите длительность фильма в секундах: '))
-hours = time // 3600
-minutes = (time - hours * 3600) // 60
-seconds = time - hours * 3600 - minutes * 60 # 1 способ
-sec = time % 60 # 2 способ
-print(f'{"%.2d" % hours}:{"%.2d" % minutes}:{"%.2d" % seconds}') # 1 способ
-print(f'{"%.2d" % hours}:{"%.2d" % minutes}:{"%.2d" % sec}') # 2 способ
